23ae59884988623778d6b3c5dc791212246b814b
aolivapalencia-rgb <300664182+aolivapalencia-rgb@users.noreply.github.com>
2026-07-07 13:15:12 -0600

    Agregar service worker

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "adorapp-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./css/styles.css?v=100",
+  "./data/songs.js",
+  "./js/app.js?v=100",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
