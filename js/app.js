acfd904011d1c89189e38d2bc43f560f1830fb30
aolivapalencia-rgb <300664182+aolivapalencia-rgb@users.noreply.github.com>
2026-07-07 15:00:40 -0600

    Agregar botón para añadir cantos al planificador

--- js/app.js
+++ js/app.js
@@ -82,7 +82,9 @@ function openSong(songId) {
       <button class="favorite-btn detail-heart" data-id="${selectedSong.id}">
         ${isFavorite ? "❤️" : "🤍"}
       </button>
-
+<button id="addToPlanBtn" class="planner-action">
+    ➕ Agregar al culto
+</button>
       <h2>${selectedSong.title}</h2>
       <p class="song-meta"><strong>Autor:</strong> ${selectedSong.artist}</p>
       <p class="song-meta"><strong>Categoría:</strong> ${selectedSong.category}</p>
